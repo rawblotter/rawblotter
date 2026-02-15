3a3d1887a274fda83ed8808a98bafccda473abad
rawblotter <102633323+rawblotter@users.noreply.github.com>
2026-02-15 05:15:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ const cartBtn = document.getElementById('cartBtn');
 
 let expanded = false;
 
-/* FILTER FUNCTION */
+/* FILTER SYSTEM */
 filters.forEach(btn => {
     btn.addEventListener('click', () => {
-        document.querySelector('.filter.active').classList.remove('active');
+
+        filters.forEach(f => f.classList.remove('active'));
         btn.classList.add('active');
 
         const category = btn.dataset.filter;
 
         products.forEach(product => {
-            if (category === "all" || product.dataset.category === category) {
+            if (product.dataset.category === category) {
                 product.style.display = "block";
             } else {
                 product.style.display = "none";
